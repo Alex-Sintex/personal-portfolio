2b1f47d793d56d8037e597e23fb56b4431e8c2a3
Alex-Sintex <60632811+Alex-Sintex@users.noreply.github.com>
2025-09-22 11:17:44 -0600

    New Laravel practices added

---
+++ laravel/blade/resources/views/_components/card.blade.php
@@ -0,0 +1,5 @@
+<div class="card">
+    <h3>{{ $title }}</h3>
+    <img src="{{ asset('assets/img/robot.jpg') }}" alt="example" width="128">
+    <p>{{ $content }}</p>
+</div>
---
+++ laravel/crud/resources/views/note/edit.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+    <a href="{{ route('note.index') }}">Back</a>
+    <form method="POST" action="{{ route('note.update', $note->id) }}">
+        @method('PUT')
+        @csrf
+        <label for="title">Title:</label>
+        <input type="text" name="title" value="{{ $note->title }}">
+        @error('title')
+            <p style="color:red;">{{ $message }}</p>
+        @enderror
+        <label for="description">Description:</label>
+        <input type="text" name="description" value="{{ $note->description }}">
+        @error('description')
+            <p style="color:red;">{{ $message }}</p>
+        @enderror
+        <input type="submit" value="Update">
+    </form>
+@endsection
