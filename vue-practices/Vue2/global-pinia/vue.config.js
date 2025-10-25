5b4729689eb4761f4c5514ff24f01e1adc45cb03
Alex-Sintex <60632811+Alex-Sintex@users.noreply.github.com>
2025-10-24 20:27:19 -0600

    Added new practices with Vuex and Pinia in Vue 3

--- vue-practices/Vue2/global-pinia/vue.config.js
+++ vue-practices/Vue2/global-pinia/vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("@vue/cli-service");
 
 module.exports = defineConfig({
   devServer: {
-    // Enable Websocket connection
+    // Enable websocket connection
     client: {
       webSocketURL: "ws://localhost:8080/ws",
     },
